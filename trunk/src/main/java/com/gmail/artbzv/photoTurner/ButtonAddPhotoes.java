0f435b529adb3fb96eb16197bb5a8db4db580e65
artbzv <artbzv@a4eb735b-84ce-7601-c11b-3d2d45e86d4a>
2012-01-11 06:45:48 +0000

    ButtonAddPhotoes is restored

---
+++ trunk/src/main/java/com/gmail/artbzv/photoTurner/ButtonAddPhotoes.java
@@ -0,0 +1,22 @@
+package com.gmail.artbzv.photoTurner;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonAddPhotoes extends JButton implements ActionListener{
+
+    ButtonAddPhotoes(){
+        setText("<html>Добавить <p>фотки");
+        setIcon(new ImageIcon("icoAdd32x32.png"));
+        setPreferredSize(new Dimension(MainFrame.buttonsWidth, MainFrame.buttonsHeight));
+        addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        new Thread(new Loader()).start();
+
+    }
+}
